7201c5c8d3a4c6b0e51deb8416d9308c75908098
ShonP40 <13995143+ShonP40@users.noreply.github.com>
2022-02-22 20:12:12 +0200

    ESPHome - Container Updater

---
+++ ESPHome - Container Updater.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Author: https://github.com/ShonP40
+# Repository: https://github.com/ShonP40/Nabu-Casa-Updaters
+# Date: 2022-02-22
+
+printf "\nWelcome to the ESPHome Container Updater!\n\n"
+
+if [[ ! -f docker-compose.yml ]]
+then
+    printf "docker-compose.yml not found!\n\n"
+    printf "Please run this updater in the same directory as your docker-compose file!\n\n"
+    exit
+fi
+
+currentVer=$(docker exec -it esphome esphome version | grep -Po "(\d+\.)+\d+")
+
+newestVer=$(curl --silent "https://api.github.com/repos/esphome/esphome/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+
+if [[ $currentVer == $newestVer ]]
+then
+    printf "You're already running the latest version of ESPHome ($currentVer)\n\n"
+    exit
+else
+    printf "Your current ESPHome version is: $currentVer\n\n"
+fi
+printf "The latest ESPHome release is: $newestVer\n\n"
+
+printf "Would you like to update ESPHome (y/n)? "
+read answer
+if [ "$answer" != "${answer#[Yy]}" ]
+then
+    printf " "
+    docker-compose pull
+
+    printf " "
+    docker-compose down
+
+    printf " "
+    docker-compose up --detach
+
+    printf "\n\nESPHome has been updated and restarted successfully!\n\n"
+    printf "You may now delete your old ESPHome image safely\n\n"
+else
+    printf "ESPHome won't be updated!\n\n"
+fi
